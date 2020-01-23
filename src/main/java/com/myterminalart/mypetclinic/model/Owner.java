18d5d5f2fa906f31538322175eab90f25a34594e
Gustavo I. Chalco <gustavo.chalco@gmc-soft.com>
2020-01-23 06:57:33 -0500

    agregando POJOs. Closes #2.

---
+++ src/main/java/com/myterminalart/mypetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.myterminalart.mypetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/myterminalart/mypetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.myterminalart.mypetclinic.model;
+
+public class Vet extends Person {
+}
