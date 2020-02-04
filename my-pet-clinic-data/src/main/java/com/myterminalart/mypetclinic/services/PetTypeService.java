02ad45c4bd6ef08fc88b11fb4a993a6cceaa2a60
Gustavo I. Chalco <gustavo.chalco@gmc-soft.com>
2020-02-04 06:45:28 -0500

    Se agrega MapService para PetType. Closes #29.

---
+++ my-pet-clinic-data/src/main/java/com/myterminalart/mypetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.myterminalart.mypetclinic.services;
+
+import com.myterminalart.mypetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ my-pet-clinic-data/src/main/java/com/myterminalart/mypetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,35 @@
+package com.myterminalart.mypetclinic.services.map;
+
+import com.myterminalart.mypetclinic.model.PetType;
+import com.myterminalart.mypetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType t) {
+        return super.save(t);
+    }
+
+    @Override
+    public void delete(PetType t) {
+        super.delete(t);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
