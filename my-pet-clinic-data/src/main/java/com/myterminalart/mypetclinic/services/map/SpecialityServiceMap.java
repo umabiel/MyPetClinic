27ac10bc676f151d22e1ff257f7abd944e928b0d
Gustavo I. Chalco <gustavo.chalco@gmc-soft.com>
2020-02-06 05:41:18 -0500

    Se agrega implementacion de mapa de Especialidades. Closes #31.

---
+++ my-pet-clinic-data/src/main/java/com/myterminalart/mypetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,35 @@
+package com.myterminalart.mypetclinic.services.map;
+
+import com.myterminalart.mypetclinic.model.Speciality;
+import com.myterminalart.mypetclinic.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialtiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return super.save(speciality);
+    }
+
+    @Override
+    public void delete(Speciality t) {
+        super.delete(t);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
