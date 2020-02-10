b3eaa53a1a05f8943b1c59c749dfca17574da7b0
Gustavo I. Chalco <gustavo.chalco@gmc-soft.com>
2020-02-10 06:27:43 -0500

    Se actualiza Entidad Vets (Veterinarios) con anotaciones para JPA. Closes #37.

--- my-pet-clinic-data/src/main/java/com/myterminalart/mypetclinic/model/Vet.java
+++ my-pet-clinic-data/src/main/java/com/myterminalart/mypetclinic/model/Vet.java
@@ -1,10 +1,19 @@
 package com.myterminalart.mypetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "vet_specialties",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "specialties_id")
+    )
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
