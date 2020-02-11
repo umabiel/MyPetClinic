5f4f1f34df7e3ad52241a367bd6826e9e9b7cfc6
Gustavo I. Chalco <gustavo.chalco@gmc-soft.com>
2020-02-11 05:36:40 -0500

    Se implementa Sevicios JPA para PetType. Closes #42.

---
+++ my-pet-clinic-data/src/main/java/com/myterminalart/mypetclinic/services/sdjpa/PetTypeSDJpaService.java
@@ -0,0 +1,46 @@
+package com.myterminalart.mypetclinic.services.sdjpa;
+
+import com.myterminalart.mypetclinic.model.PetType;
+import com.myterminalart.mypetclinic.repositories.PetTypeRepository;
+import com.myterminalart.mypetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class PetTypeSDJpaService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeSDJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return petTypeRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return petTypeRepository.save(petType);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        petTypeRepository.delete(petType);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petTypeRepository.deleteById(aLong);
+    }
+}
